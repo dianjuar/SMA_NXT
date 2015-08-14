433ea94a73a7db461cbc7833da844034d553f770
dianjuar <dianjuar@gmail.com>
2015-08-14 15:55:53 -0430

    Empezando de nuevo.

--- src/main/SMA_NXT.java
+++ src/main/SMA_NXT.java
@@ -5,7 +5,8 @@ import lejos.nxt.Button;
 
 public class SMA_NXT {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		robot yo = new robot();
 
 		Button.waitForPress();
